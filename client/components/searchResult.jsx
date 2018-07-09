b53838bcbab9ea676117110dd0022035c03bb9bd
Ryan Trontz <rtrontz@gmail.com>
2018-07-09 09:37:44 -0700

    ready to add authentication workflow

--- client/components/searchResult.jsx
+++ client/components/searchResult.jsx
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { createMarkup } from './../utils';
 
 
 const SearchResult = (props) => (
   <div className="searchResult">
     <input id={props.checkId} type="checkbox"/>
     <div className="searchDetail">
-      <p>Title: {props.name}</p>
-      <p>URL {props.url}</p>
-      <p>Description: {props.description}</p>
+      <p>Title: <span dangerouslySetInnerHTML={createMarkup(props.htmlTitle)} /></p>
+      <p>URL <a href={props.link}>{props.link}</a></p>
+      <p>Description: <span dangerouslySetInnerHTML={createMarkup(props.htmlSnippet)} /></p>
     </div>
   </div>
 );
